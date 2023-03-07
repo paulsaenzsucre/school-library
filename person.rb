6f23273fc955a87a802147aea674d9b0d0950f4e
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-03-07 18:35:19 -0500

    Fix linters errors.

--- person.rb
+++ person.rb
@@ -1,9 +1,8 @@
-class Person
-  @@person_num = 0
+require 'securerandom'
 
+class Person
   def initialize(age, name = 'Unknown', parent_permission: true)
-    @@person_num += 1
-    @id = @@person_num
+    @id = SecureRandom.uuid
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -18,7 +17,7 @@ class Person
 
   private
 
-  def is_of_age?
+  def of_age?
     @age >= 18
   end
 end
