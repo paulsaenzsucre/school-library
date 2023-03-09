a7add41d835d3ccee70e139e948f06a29df75b28
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-03-09 03:18:19 -0500

    Create a program to showcase new functionality.

---
+++ main.rb
@@ -0,0 +1,12 @@
+require_relative 'classes/person'
+require_relative 'classes/capitalize_decorator'
+require_relative 'classes/trimmer_decorator'
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
