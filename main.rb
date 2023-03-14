1cafeb96a2467e40b9d828bc275a5e024e6d0c23
shinhyo26okazaki <shibo6937@gmail.com>
2023-03-14 11:24:46 -0400

    Add missing option in the menu

--- main.rb
+++ main.rb
@@ -12,6 +12,12 @@ def selected_menu_option
   puts '1 - List all books.'
   puts '2 - List all people.'
   puts '3 - Create a person,'
+  puts '4 - Create a book.'
+  puts '5 - Create a rental.'
+  puts '6 - List all rentals for a given person id.'
+  puts "7 - Exit.\n"
+
+  get_option_selected(1, 7)
 end
 
 
