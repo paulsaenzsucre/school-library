fd658375aafb159c144ab29dc455a7579b52befd
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-03-14 10:19:22 -0500

    Refactor main: addin selected_menu_option.

--- main.rb
+++ main.rb
@@ -6,4 +6,19 @@ def main
   puts 'Thanks for using this app!'
 end
 
+def selected_menu_option
+  puts '****** SCHOOL LIBRARY ******'
+  puts "\nPlease choose an option by entering a number:\n"
+  puts '1 - List all books.'
+  puts '2 - List all people.'
+  puts '3 - Create a person,'
+end
+
+
+def wait_user
+  puts 'Press any key to continue...'
+  gets.chomp
+  puts ''
+end
+
 main
